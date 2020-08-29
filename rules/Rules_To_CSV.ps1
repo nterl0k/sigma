6120f80c6430c8803431f0b0f013839df491e6e9
nterl0k <38897662+nterl0k@users.noreply.github.com>
2020-08-29 16:32:54 -0400

    Convert rules to CSV PS script added

---
+++ rules/Rules_To_CSV.ps1
@@ -0,0 +1,41 @@
+﻿
+Try{
+    Import-Module powershell-yaml -ErrorAction Stop
+}
+Catch{
+    Write-Host "Unable to import PowerShell-Yaml Module. Trying to install." -ForegroundColor Yellow
+    Try{
+        Install-Module powershell-yaml
+        }
+    Catch{
+        Write-Host "Unable to install PowerShell-Yaml Module. `n`
+        `nPlease visit `"https://www.powershellgallery.com/packages/powershell-yaml`" and install module." -ForegroundColor Red
+        Pause
+        Exit
+        }
+}
+$YamlArray = @()
+$Rules = Get-ChildItem .\*.yml -r | ?{$_.Attributes -ne "Directory"}
+
+Foreach($Rule in $Rules){ 
+    $YamlArrayT = "" | Select Name,Filename,ID,Description,Status,Tags,Product,Service,References
+    $YamlRead = get-content $Rule.FullName | ConvertFrom-Yaml -Ordered
+    
+    $YamlArrayT.Name = $YamlRead.title
+    $YamlArrayT.Filename = $Rule.Name    
+    $YamlArrayT.ID = $YamlRead.id
+    $YamlArrayT.Description = $YamlRead.description
+    $YamlArrayT.status = $YamlRead.status
+    $YamlArrayT.tags = $YamlRead.tags -join "`n"
+    $YamlArrayT.product = $YamlRead.logsource.product -join "`n"
+    $YamlArrayT.service = $YamlRead.logsource.service -join "`n"
+    $YamlArrayT.references = $YamlRead.references -join "`n"
+
+
+    $YamlArray += $YamlArrayT
+
+}
+
+$YamlArray | Export-Csv -NoTypeInformation -Force .\Rules.csv
+#$YamlArray | ConvertTo-Html | Out-File .\Rules.html
+
