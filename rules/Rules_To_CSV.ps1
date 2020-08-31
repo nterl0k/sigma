cce3c9cf6760c4da2913ded92f33f59f99309878
nterl0k <38897662+nterl0k@users.noreply.github.com>
2020-08-31 13:44:53 -0400

    Add a few error and config validations

--- rules/Rules_To_CSV.ps1
+++ rules/Rules_To_CSV.ps1
@@ -1,26 +1,29 @@
-﻿
 Try{
     Import-Module powershell-yaml -ErrorAction Stop
 }
 Catch{
     Write-Host "Unable to import PowerShell-Yaml Module. Trying to install." -ForegroundColor Yellow
     Try{
-        Install-Module powershell-yaml
+        #OverRide Default PS TLS settings
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+        Install-Module powershell-yaml -ErrorAction Stop
         }
     Catch{
         Write-Host "Unable to install PowerShell-Yaml Module. `n`
         `nPlease visit `"https://www.powershellgallery.com/packages/powershell-yaml`" and install module." -ForegroundColor Red
         Pause
-        Exit
+        quit
         }
 }
 $YamlArray = @()
 $Rules = Get-ChildItem .\*.yml -r | ?{$_.Attributes -ne "Directory"}
 
 Foreach($Rule in $Rules){ 
+    Write-Progress -Activity "Parsing $($Rules.Count) files" -CurrentOperation "$($Rule.Name)" -PercentComplete (($Rules.IndexOf($Rule) / $Rules.Count)*100)
+
     $YamlArrayT = "" | Select Name,Filename,ID,Description,Status,Tags,Product,Service,References
     $YamlRead = get-content $Rule.FullName | ConvertFrom-Yaml -Ordered
-    
+
     $YamlArrayT.Name = $YamlRead.title
     $YamlArrayT.Filename = $Rule.Name    
     $YamlArrayT.ID = $YamlRead.id
@@ -36,6 +39,7 @@ Foreach($Rule in $Rules){
 
 }
 
+Write-Progress -Activity "Parsing $($Rules.Count) files" -Completed
+
 $YamlArray | Export-Csv -NoTypeInformation -Force .\Rules.csv
 #$YamlArray | ConvertTo-Html | Out-File .\Rules.html
-
